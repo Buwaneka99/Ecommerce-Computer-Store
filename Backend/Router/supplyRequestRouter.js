1d75a06df49c16cb3c33fc6d2e6fc80d1c272aca
Buwaneka Wijesinghe <128914545+Buwaneka99@users.noreply.github.com>
2025-03-01 20:01:27 +0530

    new create

---
+++ Backend/Router/supplyRequestRouter.js
@@ -0,0 +1,2 @@
+import express from 'express';
+import { createSupplyRequest, getSupplyRequest, getSupplyRequests, updateSupplyRequest, deleteSupplyRequest } from '../Controllers/supplyRequestController.js';
