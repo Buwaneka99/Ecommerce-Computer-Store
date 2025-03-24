dc075c790ee4370c8e91b2dbff4b726205f96088
Buwaneka Wijesinghe <128914545+Buwaneka99@users.noreply.github.com>
2025-03-25 01:36:57 +0530

    update app.js and Login

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Importing the main components
-//import Login from './Pages/Login';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <Router>
-      <Route path="/login" element={Login} />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      /</Routes>
     /</Router>
   );
 }
--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -25,3 +25,4 @@ const Login = () => {
     </div>
   );
 }
+export default Login;
