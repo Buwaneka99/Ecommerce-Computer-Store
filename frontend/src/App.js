d1a13e8b3f52f76ee03c26df1cbb476a84462739
Dilmi Jayanetthi <dilmijayanetthi66@gmail.com>
2025-03-28 06:28:58 +0530

    first one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,11 @@ import Register from "./Pages/Register";
 import './App.css';
 
 
+import Inventory from "./pages/inventory/inventory/Inventory";
+import AddProduct from "./pages/inventory/inventory/AddProduct";
+import ProductsList from "./pages/inventory/inventory/ProductsList";
+import EditProduct from "./pages/inventory/inventory/EditProduct";
+
 function App() {
   return (
     <Router>
@@ -17,6 +22,26 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+
+
+        {/* Supply Management Routes */}
+        <Route path="/dashboard/supply" element={<Supply />} />
+        <Route path="/dashboard/supply/add" element={<AddSupplier />} />
+        <Route path="/dashboard/supply/edit/:id" element={<EditSupplier />} />
+        <Route path="/dashboard/supply/list" element={<SupplierList />} />
+        <Route path="/dashboard/supply/request-list" element={<SupplierRequest />} />
+
+          {/* Product management */}
+                <Route path="/dashboard/products" element={<Inventory />} />
+                <Route path="/dashboard/products/add" element={<AddProduct />} />
+                <Route path="/dashboard/products/list" element={<ProductsList />} />
+                <Route path="/dashboard/products/edit/:id" element={<EditProduct />} />
+
+
+        {/* 404 Not Found Route */}
+        <Route path="*" element={<NotFound />} />
+        
+
       </Routes>
     </Router>
   );
