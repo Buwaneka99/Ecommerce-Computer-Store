b9f3c944379b85d5fcbaed6d649f9fe832edef11
Buwaneka Wijesinghe <128914545+Buwaneka99@users.noreply.github.com>
2025-03-28 15:43:18 +0530

    Update HomeNavBar.js

--- frontend/src/Components/HomeNavBar.js
+++ frontend/src/Components/HomeNavBar.js
@@ -3,12 +3,10 @@ import { useState } from "react";
 import logo from "../Assets/newlogo.png";
 import { FaUserCircle } from "react-icons/fa";
 import {
-  AiFillTag,
-  AiOutlineClose,
   AiOutlineMenu,
   AiOutlineSearch,
 } from "react-icons/ai";
-import { BsFillCartFill, BsFillSaveFill } from "react-icons/bs";
+import { BsFillCartFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { RiLogoutBoxLine } from "react-icons/ri";
 import { useGlobalReefetch } from "../Store/Store";
